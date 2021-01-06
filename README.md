d0484b177c98b413a44af295e90f1302fd3271dc
ta_mu <ta_mu@hunliji.com>
2021-01-06 17:38:11 +0800

    重置ignore

---
+++ README.md
@@ -0,0 +1,8 @@
+使用Nodejs编写的博客系统  
+html-test为客户端  
+启动方式   
+`npx http-server -p 8001 --proxy http://localhost:8000`
+
+node-blog为服务端
+启动方式
+`npm dev`
---
+++ node-blog/src/main/index.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const serverHandler = require('./handler/server-handler')
+const exceptionHandler = require('./handler/exception-handler')
+const PORT = 8000
+
+// 统一异常处理
+process.on('uncaughtException', e => {
+
+});
+const server = http.createServer(async (req, res) => {
+  try {
+    await serverHandler(req, res)
+  } catch (e) {
+    console.error(e)
+    exceptionHandler(e, req, res)
+  }
+})
+server.listen(PORT)
+console.log('启动成功....')
